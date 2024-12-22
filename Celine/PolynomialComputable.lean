c905c54f01a1bff2f57fe99aab1d60461df675fa
BPRocket <benjamin.przybocki@gmail.com>
2024-12-22 14:37:21 -0600

    reduce univariate rational functions

--- Celine/PolynomialComputable.lean
+++ Celine/PolynomialComputable.lean
@@ -1,4 +1,6 @@
-abbrev UniPolynomial := List Int
+import Mathlib.Data.Rat.Init
+
+abbrev UniPolynomial := List Rat
 
 def all_zeros_uni (a : UniPolynomial) : Bool :=
   match a with
@@ -16,7 +18,7 @@ def add_uni (a b : UniPolynomial) : UniPolynomial :=
   | _, [] => a
   | x :: xs, y :: ys => canon_uni ((x + y) :: (add_uni xs ys))
 
-def scale_uni (a : UniPolynomial) (c : Int) : UniPolynomial :=
+def scale_uni (a : UniPolynomial) (c : Rat) : UniPolynomial :=
   if c = 0 then [] else match a with
   | [] => []
   | x :: xs => c*x :: (scale_uni xs c)
@@ -25,6 +27,27 @@ def mult_uni (a b : UniPolynomial) : UniPolynomial :=
   match a, b with
   | [], _ | _, [] => []
   | x1::xs, _ => add_uni (scale_uni b x1) (0::(mult_uni xs b))
+
+def n_copies {α : Type} (x : α) (n : Nat) : List α :=
+  match n with
+  | 0 => []
+  | Nat.succ n' => (n_copies x n').concat x
+
+def div_uni_aux (a b q r : UniPolynomial) (b_nonempty : b.length > 0) : UniPolynomial × UniPolynomial :=
+  if h : r.length ≥ b.length then
+  let t := (n_copies 0 (r.length - b.length)).concat (r[r.length-1] / b[b.length-1])
+  let q' := add_uni q t
+  let r' := (add_uni r (scale_uni (mult_uni t b) (-1))).take (r.length - 1)
+  div_uni_aux a b q' r' b_nonempty
+  else (q,r)
+
+def div_uni (a b : UniPolynomial) : UniPolynomial × UniPolynomial :=
+  if h : (canon_uni b).length > 0 then div_uni_aux (canon_uni a) (canon_uni b) [] (canon_uni a) h
+  else ([],[])
+
+def gcd_uni (a b : UniPolynomial) : UniPolynomial :=
+  if h : b.length > 0 ∧ (canon_uni b).length > 0 then gcd_uni b ((div_uni a b).snd.take (b.length - 1)) else a
+  termination_by b.length
 
 abbrev BiPolynomial := List UniPolynomial
 
@@ -57,19 +80,23 @@ def mult_bi (a b : BiPolynomial) : BiPolynomial :=
 abbrev UniRatFunc := UniPolynomial × UniPolynomial
 
 def is_zero_uni_ratfunc (a : UniRatFunc) : Bool :=
-  canon_uni a.fst = []
+  canon_uni a.fst = [] ∧ canon_uni a.snd ≠ []
+
+def reduce_uni_ratfunc (a : UniRatFunc) : UniRatFunc :=
+  let g := gcd_uni a.fst a.snd
+  ((div_uni a.fst g).fst, (div_uni a.snd g).fst)
 
 def add_uni_ratfunc (a b : UniRatFunc) : UniRatFunc :=
-  (add_uni (mult_uni a.fst b.snd) (mult_uni a.snd b.fst), mult_uni a.snd b.snd)
+  reduce_uni_ratfunc (add_uni (mult_uni a.fst b.snd) (mult_uni a.snd b.fst), mult_uni a.snd b.snd)
 
 def mult_uni_ratfunc (a b : UniRatFunc) : UniRatFunc :=
-  (mult_uni a.fst b.fst, mult_uni a.snd b.snd)
+  reduce_uni_ratfunc (mult_uni a.fst b.fst, mult_uni a.snd b.snd)
 
 def neg_uni_ratfunc (a : UniRatFunc) : UniRatFunc :=
   mult_uni_ratfunc ([-1], [1]) a
 
 def div_uni_ratfunc (a b : UniRatFunc) : UniRatFunc :=
-  (mult_uni a.fst b.snd, mult_uni a.snd b.fst)
+  reduce_uni_ratfunc (mult_uni a.fst b.snd, mult_uni a.snd b.fst)
 
 def inverse_uni_ratfunc (a : UniRatFunc) : UniRatFunc :=
   div_uni_ratfunc ([1],[1]) a
