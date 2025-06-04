e62fdd978046c3aa17d68028f91d98575f22c8f9
BPRocket <benjamin.przybocki@gmail.com>
2025-06-04 13:10:14 +0100

    clean up

--- Celine.lean
+++ Celine.lean
@@ -1,8 +1,6 @@
 -- This module serves as the root of the `Celine` library.
 -- Import modules here that should be built as part of the library.
-import Celine.Basic
 import Celine.CelineAlg
--- import Celine.FloorDivision
 import Celine.GaussianElim
--- import Celine.Misc
 import Celine.PolynomialComputable
+import Celine.RecurrenceSum
